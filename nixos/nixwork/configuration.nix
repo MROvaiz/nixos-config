9a9535e43619b450263315f9ac6b8b5b939ca3a0
MROvaiz <rahilovaiz@gmail.com>
2024-06-04 21:39:39 +0530

    nixwork fixes

--- nixos/nixwork/configuration.nix
+++ nixos/nixwork/configuration.nix
@@ -67,38 +67,17 @@
     };
   };
 
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-  # This will additionally add your inputs to the system's legacy channels
-  # Making legacy nix commands consistent as well, awesome!
-  # Set the nixPath to include the unstable nixpkgs from the flake inputs
-  nix.nixPath = [
-    "nixpkgs=${inputs.nixpkgs}/nixpkgs"
-    # "/etc/nixos/path"
-  ];
-  # Use lib.mapAttrs' to create environment.etc entries for the nix path
-  environment.etc = let
-    nixpkgsUnstablePath = "${inputs.nixpkgs}/nixpkgs";
-  in
-    lib.mapAttrs'
-    (name: value: {
-      name = "nix/path/${name}";
-      value.source =
-        if name == "nixpkgs"
-        then nixpkgsUnstablePath
-        else value.flake;
-    })
-    config.nix.registry;
-
-  nix = {
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
     settings = {
       # Enable flakes and new 'nix' command
-      experimental-features = ["nix-command" "flakes"];
-      # Deduplicate and optimize nix store
+      experimental-features = "nix-command flakes";
+      # Opinionated: disable global registry
+      flake-registry = "";
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
       auto-optimise-store = true;
-      warn-dirty = false;
       # Substituters for little faster download
       substituters = [
         "https://nix-community.cachix.org"
@@ -110,15 +89,20 @@
         "fufexan.cachix.org-1:LwCDjCJNJQf5XD2BV+yamQIMZfcKWR9ISIFy5curUsY="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
-      trusted-users = ["root" "@wheel"];
     };
+    # Opinionated: disable channels
+    channel.enable = false;
+
+    # Opinionated: make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+
     gc = {
       automatic = true;
       dates = "weekly";
       options = "--delete-older-than 10d";
     };
   };
-
   # FIXME: Add the rest of your current configuration
 
   # TODO: Set your hostname
