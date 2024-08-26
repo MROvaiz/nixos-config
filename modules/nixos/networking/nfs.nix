8b47a169237a2a25cee6d2a54cc636f2b3b515ef
MROvaiz <rahilovaiz@gmail.com>
2024-08-27 00:26:29 +0530

    nfs test

--- modules/nixos/networking/nfs.nix
+++ modules/nixos/networking/nfs.nix
@@ -6,12 +6,11 @@
   services.nfs.server = lib.mkIf (config.networking.hostName == "nixbin") {
     enable = true;
     exports = ''
-      /export         192.168.191.124(rw,fsid=0,no_subtree_check)
-      /export/data    192.168.191.124(rw,nohide,insecure,no_subtree_check)
+      /export/data    nixwork(ro,no_subtree_check) nixbin(ro,no_subtree_check) 10.147.17.67(ro,no_subtree_check) 10.147.17.231(ro,no_subtree_check)
     '';
   };
   fileSystems."/mnt/data" = lib.mkIf (config.networking.hostName == "nixwork") {
-    device = "192.168.191.67:/export/data/";
+    device = "nixbin:/export/data/";
     fsType = "nfs";
     options = ["x-systemd.automount" "noauto" "x-systemd.after=network-online.target" "x-systemd.mount-timeout=90"];
   };
