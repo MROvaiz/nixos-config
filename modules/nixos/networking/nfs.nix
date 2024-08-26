43e61361e3b35b26a56b1a3f57db07ca35f6e0cb
MROvaiz <rahilovaiz@gmail.com>
2024-08-26 20:06:40 +0530

    nfs with zerotier

--- modules/nixos/networking/nfs.nix
+++ modules/nixos/networking/nfs.nix
@@ -6,12 +6,12 @@
   services.nfs.server = lib.mkIf (config.networking.hostName == "nixbin") {
     enable = true;
     exports = ''
-      /export         nixwork(rw,fsid=0,no_subtree_check)
-      /export/data    nixwork(rw,nohide,insecure,no_subtree_check)
+      /export         192.168.191.124(rw,fsid=0,no_subtree_check)
+      /export/data    192.168.191.124(rw,nohide,insecure,no_subtree_check)
     '';
   };
   fileSystems."/mnt/data" = lib.mkIf (config.networking.hostName == "nixwork") {
-    device = "nixbin:/export/data/";
+    device = "192.168.191.67:/export/data/";
     fsType = "nfs";
     options = ["x-systemd.automount" "noauto" "x-systemd.after=network-online.target" "x-systemd.mount-timeout=90"];
   };
