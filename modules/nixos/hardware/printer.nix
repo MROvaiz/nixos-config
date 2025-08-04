a00f1b7bba9a0c2eab7663590f77c71f2048183b
MROvaiz <rahilovaiz@gmail.com>
2025-08-04 19:04:54 +0530

    scanner for printer

--- modules/nixos/hardware/printer.nix
+++ modules/nixos/hardware/printer.nix
@@ -8,4 +8,13 @@
     enable = true;
     drivers = [pkgs.epson-201401w];
   };
+  hardware.sane.enable = true;
+  hardware.sane.extraBackends = [pkgs.utsushi];
+  services.udev.packages = [pkgs.utsushi];
+  environment.systemPackages = with pkgs; [
+    imagemagick
+    ghostscript
+    libtiff
+    sane-frontends
+  ];
 }
