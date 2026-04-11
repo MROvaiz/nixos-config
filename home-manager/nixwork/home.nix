286ec723b0154a35c69837336dd7de4007ba8a79
MROvaiz <rahilovaiz@gmail.com>
2026-04-11 13:06:13 +0530

    clean up

--- home-manager/nixwork/home.nix
+++ home-manager/nixwork/home.nix
@@ -30,7 +30,9 @@
     };
   };
 
+  # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
 }
